07bde677fea5bcc0976afdeb01feeae60d9bc75b
Will Scott <wscott@ucsb.edu>
2026-05-08 20:22:15 -0700

    yuh

--- card_list.h
+++ card_list.h
@@ -11,54 +11,55 @@ using namespace std;
 
 
 class cardBST {
-
- public:
-	    cardBST();                   // constructor
-	    ~cardBST();                  // destructor
-	    // TODO: Eventually insert should build a card once and compare using card operators.
-	    bool insert(string line);     // insert value; return false if duplicate
-
-	    // TODO: Print each card on its own line for the final hand output.
-	    void printInOrder() const;       // print tree data in-order to cout
-
-	    // TODO: Change this to take a card instead of an int value.
-	    bool contains(int value) const;  // true if value is in tree
-	    // TODO: Change this to take a card instead of an int value.
-	    bool remove(int value);                    // deletes the Node containing the given value from the tree
-
-	 private:
-
+private:
     struct Node {
 	card* c;
 	Node *left, *right, * parent;
 	    Node(string v) : left(0), right(0), parent(0) {
 	        c = new card(v);
 	    }
-	    };
+	};
 
-	    Node *root;
+	Node *root;
 
-	    // TODO: Change this to search by card instead of an int value.
-	    Node* getNodeFor(int value, Node* n) const; // IMPLEMENT THIS FIRST: returns the node for a given value or NULL if none exists
-	    void clear(Node *n); // for destructor
-	    // TODO: Eventually pass a card through insertion instead of a separate string and int.
-	    bool insert(string value, int val, Node *n); // note overloading names for simplicity
-	    void printInOrder(Node *n) const;
+    // TODO: Change this to search by card instead of an int value.
+    Node* getNodeFor(int value, Node* n) const; // IMPLEMENT THIS FIRST: returns the node for a given value or NULL if none exists
+    void clear(Node *n); // for destructor
+    // TODO: Eventually pass a card through insertion instead of a separate string and int.
+    bool insert(string value, int val, Node *n); // note overloading names for simplicity
+    void printInOrder(Node *n) const;
 
-	    // TODO: These can be changed to work from a Node* or card instead of an int value.
-	    Node* getSuccessorNode(int value) const;   // returns the Node containing the successor of the given value
-	    Node* getPredecessorNode(int value) const; // returns the Node containing the predecessor of the given value 
+    // TODO: These can be changed to work from a Node* or card instead of an int value.
+    Node* getSuccessorNode(int value) const;   // returns the Node containing the successor of the given value
+    Node* getPredecessorNode(int value) const; // returns the Node containing the predecessor of the given value 
+
+
 public:
-    class iterator
-    {
+    class iterator {
     private:
         Node* current;
     public:
         iterator(Node* ptr) : current(ptr) {}
         card* operator*() const;
-        cardBST::iterator& cardBST::iterator::operator++();
-        cardBST::iterator& cardBST::iterator::operator--();
+        iterator& operator++();
+        iterator& operator--();
     };
+
+    cardBST();                   // constructor
+    ~cardBST();                  // destructor
+
+    // TODO: Eventually insert should build a card once and compare using card operators.
+    bool insert(string line);     // insert value; return false if duplicate
+
+    // TODO: Print each card on its own line for the final hand output.
+    void printInOrder() const;       // print tree data in-order to cout
+
+    // TODO: Change this to take a card instead of an int value.
+    bool contains(int value) const;  // true if value is in tree
+
+    // TODO: Change this to take a card instead of an int value.
+    bool remove(int value);                    // deletes the Node containing the given value from the tree
+
 
     iterator begin() const;
     iterator end() const;
