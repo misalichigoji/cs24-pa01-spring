f47210caad84884d9ad332d663c24f1669561391
misalichigoji <misali@ucsb.edu>
2026-05-08 20:57:47 -0700

    !

--- card_list.h
+++ card_list.h
@@ -22,14 +22,12 @@ private:
 
 	Node *root;
 
-    // TODO: Change this to search by card instead of an int value.
-    Node* getNodeFor(int value, Node* n) const; // IMPLEMENT THIS FIRST: returns the node for a given value or NULL if none exists
-    void clear(Node *n); // for destructor
-    // TODO: Eventually pass a card through insertion instead of a separate string and int.
-    bool insert(string value, int val, Node *n); // note overloading names for simplicity
-    void printInOrder(Node *n) const;
 
-    // TODO: These can be changed to work from a Node* or card instead of an int value.
+    Node* getNodeFor(int value, Node* n) const; 
+    void clear(Node *n); // for destructor
+    bool insert(string value, int val, Node *n); 
+
+
     Node* getSuccessorNode(int value) const;   // returns the Node containing the successor of the given value
     Node* getPredecessorNode(int value) const; // returns the Node containing the predecessor of the given value 
 
@@ -50,16 +48,11 @@ public:
     cardBST();                   // constructor
     ~cardBST();                  // destructor
 
-    // TODO: Eventually insert should build a card once and compare using card operators.
     bool insert(string line);     // insert value; return false if duplicate
 
-    // TODO: Print each card on its own line for the final hand output.
-    void printInOrder() const;       // print tree data in-order to cout
 
-    // TODO: Change this to take a card instead of an int value.
     bool contains(int value) const;  // true if value is in tree
 
-    // TODO: Change this to take a card instead of an int value.
     bool remove(int value);                    // deletes the Node containing the given value from the tree
 
 
